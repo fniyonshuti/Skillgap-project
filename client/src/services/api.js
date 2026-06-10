0e6ce9a3d9719ff590d999637c797d1931d947fa
fniyonshuti <f.niyonshut1@alustudent.com>
2026-06-10 21:23:32 +0200

    feat: added client API service layer

---
+++ client/src/services/api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
+export const api = axios.create({
+  baseURL: API_URL
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("skills_gap_token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export function getErrorMessage(error) {
+  return error.response?.data?.message || error.message || "Something went wrong.";
+}
